3b78012ead98e70e71fde682ee5aa4f34eb48b45
Daiana Palacios <daianapalacios213@gmail.com>
2025-05-15 22:03:12 -0300

    gh-pages2

--- src/config.js
+++
@@ -1,9 +0,0 @@
-export const API_URL = 'https://backend-pasantias.onrender.com';
-
-export const getAuthHeader = () => {
-    const token = localStorage.getItem('token');
-    return {
-        'Authorization': `Bearer ${token}`,
-        'Content-Type': 'application/json'
-    };
-};
